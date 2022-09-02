6b10e3dbfc307bf68f99a72c0211ec8b1be185e2
jr19089 <jr19089@melville-high.school.nz>
2022-09-02 13:14:54 +1200

    Improved Version

--- 02_Ticket_HoldersV2.py
+++ 02_Ticket_HoldersV2.py
@@ -3,5 +3,17 @@ count = 0
 MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
-    name = input("Name: ")
+    if count == 4:
+        print("You have 1 seat left.")
+    else:
+        print("You have {} seats left.".format(MAX_TICKETS - count))
+
+    name = input("Name: ").lower()
     count += 1
+
+    if name == "xxx" :
+        print("You sold {} seats and there are {} seats left.".format(count,MAX_TICKETS - count))
+    elif name == "xxx" and count == 4:
+        print("You sold 4 seats and there is 1 seat left.")
+    elif count == MAX_TICKETS:
+        print("You have sold every seat!")
