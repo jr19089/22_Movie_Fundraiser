d212b855b3c3e11cfc5c1823e920639f9649e2cf
jr19089 <jr19089@melville-high.school.nz>
2022-09-16 13:29:56 +1200

    Fully Intergrated, may have bugs

---
+++ Checkpoint_02_IntergrationV2.py
@@ -0,0 +1,52 @@
+def not_blank(question,error):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print(error)
+def int_check(question):
+    error = "Please enter a Whole Number"
+
+    Valid = False
+    while not Valid:
+        try:
+            response = int(input(question))
+            return response
+        except ValueError:
+            print(error)
+
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+    if count == 4:
+        print("You have 1 seat left.")
+    else:
+        print("You have {} seats left.".format(MAX_TICKETS - count))
+
+    name = not_blank("Name? ","This cannot be blank").lower()
+
+    if name == "xxx":
+        print("You sold {} seats and there are {} seats left.".format(count,MAX_TICKETS - count))
+        break
+    elif name == "xxx" and count == 4:
+        print("You sold 4 seats and there is 1 seat left.")
+
+    age = int_check("Age? ")
+    if age < 12:
+        print("You are too young for this movie")
+        continue
+    elif age > 130:
+        print("This is too old. There must be a mistake.")
+        continue
+
+    count += 1
+
+if count == MAX_TICKETS:
+        print("You have sold every seat!")
